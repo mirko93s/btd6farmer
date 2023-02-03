9b26985cb956026103df373fe9684e5d4cfa52b3
Linus J <30862662+linus-jansson@users.noreply.github.com>
2023-02-03 17:48:29 +0100

    cleanup

--- src/input/simulatedinput.py
+++ src/input/simulatedinput.py
@@ -1,6 +1,10 @@
 import keyboard
 import mouse
 from time import sleep
+from ..processing import monitor
+
+import static
+
 # Maybe add static in here somewhere?
 def send_key( keybind, timeout=0.1, amount=1):
     """"""
@@ -10,12 +14,15 @@ def send_key( keybind, timeout=0.1, amount=1):
 
 
 def move_mouse(location, move_timeout=0.1):
-    """"""
+    """
+        Method to move the mouse to a specific location on the screen
+        @param location: The location to move the mouse to (x, y)
+    """
     mouse.move(x=location[0], y=location[1])
     sleep(move_timeout)
 
 
-def click(location: tuple | tuple, amount=1, timeout=0.5, move_timeout=0.1, press_time=0.075):
+def click(location: tuple | tuple, amount=1, timeout=0.5, move_timeout=0.1, hold_time=0.075, _button='left'):
     """"""
     """
     Method to click on a specific location on the screen
@@ -23,7 +30,7 @@ def click(location: tuple | tuple, amount=1, timeout=0.5, move_timeout=0.1, pres
     @param amount: amount of clicks to be performed
     @param timeout: time to wait between clicks
     @param move_timeout: time to wait between move and click
-    @param press_time: time to wait between press and release
+    @param hold_time: time to wait between press and release
     """
 
     # If location is a string then assume that its a static button
@@ -31,12 +38,12 @@ def click(location: tuple | tuple, amount=1, timeout=0.5, move_timeout=0.1, pres
         location = static.button_positions[location]
     
     # Move mouse to location
-    self._move_mouse(self._scaling(location), move_timeout)
+    move_mouse(monitor.scaling(location), move_timeout)
 
     for _ in range(amount):
-        mouse.press(button='left')
-        sleep(press_time) # https://www.reddit.com/r/AskTechnology/comments/4ne2tv/how_long_does_a_mouse_click_last/ TLDR; dont click too fast otherwise shit will break
-        mouse.release(button='left')
+        mouse.press(button=_button)
+        sleep(hold_time) # https://www.reddit.com/r/AskTechnology/comments/4ne2tv/how_long_does_a_mouse_click_last/ TLDR; dont click too fast otherwise shit will break
+        mouse.release(button=_button)
         
         """
             We don't need to apply cooldown and slow down the bot on single clicks
