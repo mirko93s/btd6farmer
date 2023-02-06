2fa0d70728474a2668f830f25fb6f028a80bd77f
Linus J <30862662+linus-jansson@users.noreply.github.com>
2023-02-06 11:11:40 +0100

    Seems to work on 1080p monitors ONLY atm

--- src/recognition.py
+++ src/recognition.py
@@ -108,11 +108,14 @@ def locate_all(template_path, confidence=0.9, limit=100, region=None):
         # width & height of the template
         templateHeight, templateWidth = template.shape[:2]
 
-        # scale template
-        # Could I use monitor.scale here? Why is is scaling?
-        if monitor.width != 2560 or monitor.height != 1440:
+        # Scale template to monitor resolution
+        if monitor.width != 1920  or monitor.height != 1080:
             print("Template scaling to monitor resolution")
-            template = cv2.resize(template, dsize=(int(templateWidth/(2560/monitor.width)), int(templateHeight/(1440/monitor.height))), interpolation=cv2.INTER_CUBIC)
+            template = cv2.resize(
+                template, 
+                dsize=(int(templateWidth/(1920/monitor.width)), int(templateHeight/(1080/monitor.height))), 
+                interpolation=cv2.INTER_CUBIC
+            )
         
         # Find all the matches
         # https://stackoverflow.com/questions/7670112/finding-a-subimage-inside-a-numpy-image/9253805#9253805
