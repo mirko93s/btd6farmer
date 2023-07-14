463004c2e2e99fdf74c6ce7dfdcb43aa988ea077
mirko93s <21228368+mirko93s@users.noreply.github.com>
2023-07-14 18:20:20 +0200

    new getRound function
    It reads the round value directly from the memory so it is 100% reliable unlike ocr which breaks often in some maps.
    I didn't test every possible scenario, but it was working fine (starting,/restarting games)

--- src/Bot.py
+++ src/Bot.py
@@ -15,6 +15,7 @@ import simulatedinput
 import monitor
 import gameplan
 from logger import logger as log
+import pymem
 
 # Definently fix this 
 class Bot():
@@ -524,94 +525,23 @@ class Bot():
             still_loading = self.checkFor("loading_screen")
 
         log.debug("Out of loading screen, continuing..")
-        
-    def getRoundArea(self):
-        # Init round area dict with width and height of the round area
-        round_area = {
-            "width": 200,
-            "height": 42
-        }
-
-        # Search for round text, returns (1484,13) on 1080p
-        area = self.checkFor("round", return_cords=True, center_on_found=False) 
-        log.debug("this should be only printed once, getting round area")
-        log.debug(f"Round area found at {area}, applying offsetts")
-        
-        if area:
-            log.info("Found round area!")
-
-            # set round area to the found area + offset
-            x, y, roundwidth, roundheight = area
-            
-            # Fiddled offset, do not tuch
-            # Offset from ROUND text to round number
-            xOffset = roundwidth + 10
-            yOffset = int(roundheight * 3) - 40
-
-            round_area["top"] = y + yOffset
-            round_area["left"] = x - xOffset
-
-            return round_area
-
-        # If it cant find anything
-        log.warning("Could not find round area, setting default values")
-        default_round_area_scaled = monitor.scaling([0.7083333333333333, 0.0277777777777778]) # Use default values, (1360,30) on 1080p
-
-        # left = x, top = y
-        round_area["left"] = default_round_area_scaled[0]
-        round_area["top"] = default_round_area_scaled[1]
-        return round_area
-    
+           
 
     def getRound(self):
-        # If round area is not located yet
-        if self.round_area is None:
-            self.round_area = self.getRoundArea()
-        
-        # Setting up screen capture area
-        # The screen part to capture
-        screenshot_dimensions = {
-            'top': self.round_area["top"], 
-            'left': self.round_area["left"], 
-            'width': self.round_area["width"], 
-            'height': self.round_area["height"] + 50
-        }
+        add = 0x03096928
+        offsets = [0x40, 0xB8, 0x0, 0xC0, 0x120, 0x20, 0x20]
 
-        # Take Screenshot
-        with mss.mss() as screenshotter:
-            screenshot = screenshotter.grab(screenshot_dimensions)
-            found_text, _ocrImage = ocr.getTextFromImage(screenshot)
-            
-            if self.DEBUG:
-                from cv2 import imwrite, IMWRITE_PNG_COMPRESSION
-                def get_valid_filename(s):
-                    s = str(s).strip().replace(' ', '_')
-                    return re.sub(r'(?u)[^-\w.]', '', s)
-                imwrite(f"./DEBUG/OCR_DONE_FOUND_{get_valid_filename(found_text)}_{str(time.time())}.png", _ocrImage, [IMWRITE_PNG_COMPRESSION, 0])
+        pm = pymem.Pymem('BloonsTD6.exe')
+        gameModule = pymem.process.module_from_name(pm.process_handle, "GameAssembly.dll").lpBaseOfDll
+        address = pm.read_longlong(gameModule+add)
 
-            # Get only the first number/group so we don't need to replace anything in the string
-            if re.search(r"(\d+/\d+)", found_text):
-                found_text = re.search(r"(\d+)", found_text)
-                return int(found_text.group(0))
-            else:
-                # If the found text does not match the regex requirements, Debug and save image
-                log.warning("Found text '{}' does not match regex requirements".format(found_text))
-                
-                try:
-                    file_path =  Path(__file__).resolve().parent.parent/ "DEBUG"
-                    if not file_path.exists():
-                        Path.mkdir(file_path)
+        for offset in offsets:
+            try:
+                address = pm.read_longlong(address + offset)
+            except Exception as e:
+                print(e)
 
-                    with open(file_path/f"GETROUND_IMAGE_{str(time.time())}.png", "wb") as output_file:
-                        output_file.write(mss.tools.to_png(screenshot.rgb, screenshot.size))
-                    
-                    log.warning("Saved screenshot of what was found")
-
-                except Exception as e:
-                    log.error(e)
-                    log.warning("Could not save screenshot of what was found")
-
-                return None
+        return address + offsets[-1] -32
 
     def waitForRound(self, round) -> None:
         """Wait for a specific round to start, use this to wait until to execute a gameplan instruction"""
